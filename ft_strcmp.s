6066515db31121dd1e3b9338700d18192f752b14
makhmudego <makhmudego@yandex.ru>
2020-09-28 13:28:08 +0300

    test

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -2,6 +2,17 @@ global     _ft_strcmp
 
 section     .text
 
+_ft_strcmp:
+            xor rax, rax
 _compare:
-        cmp
+            cmp byte[rsi + rax], 0
+            je  _end
+            cmp byte[rdi + rax], 0
+            je  _end
 
+            cmp r10
+            inc rax
+            jmp _compare
+
+_end:
+            sub
