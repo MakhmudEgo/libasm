e75835fb99af08ab7000c292d197a6ebf0315d06
Makhmud Ego <makhmudego@yandex.ru>
2020-09-28 17:50:43 +0300

    28092020175041

---
+++ ft_write.s
@@ -0,0 +1,6 @@
+global      _ft_write
+
+extern ___error
+section .text
+
+_ft_write:
--- libasm.h
+++ libasm.h
@@ -7,5 +7,7 @@ int		ft_strlen(char *s);
 char	*ft_strcpy(char *dst, const char *src);
 int		ft_strcmp(const char *s1, const char *s2);
 char	*ft_strdup(const char *s1);
+int		ft_write(int fildes, const void *buf, int nbyte);
+int		ft_read(int fildes, void *buf, int nbyte);
 
 #endif
